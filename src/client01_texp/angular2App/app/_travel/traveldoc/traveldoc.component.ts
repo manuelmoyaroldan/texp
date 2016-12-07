85c238f419eea72e8bd5301e18c7085a9cdaca45
manuel <manuel.moya.roldan@gmail.com>
2016-12-07 12:55:29 +0100

    --20161207A

--- src/client01_texp/angular2App/app/_travel/traveldoc/traveldoc.component.ts
+++ src/client01_texp/angular2App/app/_travel/traveldoc/traveldoc.component.ts
@@ -19,20 +19,49 @@ import { DataListModule, DataTableModule, MenuItem, ContextMenuModule, DialogMod
         , DialogModule
     ]
     //, animations: [trigger('myAnimation', [transition(':enter', [style({ transform: 'translateX(100%), opacity: 0' }),animate('500ms', style({ transform: 'translateX(0), opacity: 1' }))])]]
+    //, animations: [
+    //    trigger('flyInOut', [
+    //        state('center', style({ transform: 'translateX(0)' })),
+    //        state('left', style({ transform: 'translateX(-100%)' })),
+    //        state('right', style({ transform: 'translateX(100%)' })),
+    //        transition('* => left, * => right, left => center, right => center, * =>center',
+    //            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)'))
+
+    //        //transition(':enter', [
+    //        //    style({ transform: 'translateX(100%)' }),
+    //        //    animate(500)
+    //        //]),
+    //        //transition(':leave', [
+    //        //    animate(500, style({ transform: 'translateX(-100%)' }))
+    //        //])
+    //    ])
+    //]    
     , animations: [
         trigger('flyInOut', [
-            //state('in', style({ transform: 'translateX(0)' })),
-            transition(':enter', [
-                style({ transform: 'translateX(100%)' }),
+            //state('center', style({ transform: 'translateX(0)' })),
+            //state('left', style({ transform: 'translateX(-100%)' })),
+            //state('right', style({ transform: 'translateX(100%)' })),
+            //transition('* => left, * => right, left => center, right => center, * =>center',
+            //    animate('500ms cubic-bezier(0.35, 0, 0.25, 1)'))
+            transition('void=>right', [
+                style({ transform: 'translateX(-100%)' }),
                 animate(500)
             ]),
-            transition(':leave', [
-                animate(500, style({ transform: 'translateX(-100%)' }))
-            ])
+            //transition('right=>void', [
+            //    animate(0, style({ transform: 'translateX(-100%)' }))
+            //]),
+            transition('void=>left', [
+                style({ transform: 'translateX(+100%)' }),
+                animate(500)
+            ]),
+            //transition('left=>void', [
+            //    animate(0, style({ transform: 'translateX(100%)' }))
+            //])
+
         ])
     ]    
     //,animations: [
-    //    trigger('translateTab', [
+    //    trigger('flyInOut', [
     //        state('left', style({ transform: 'translate3d(-100%, 0, 0)' })),
     //        state('left-origin-center', style({ transform: 'translate3d(0, 0, 0)' })),
     //        state('right-origin-center', style({ transform: 'translate3d(0, 0, 0)' })),
@@ -57,6 +86,7 @@ export class TraveldocComponent implements OnInit {
     public myparams: any;
 
     public doc_state: any = 1;
+    public doc_state_prev: any = 1;
     public show_toolbar: any = 1;
     public prev_state: any = 0;
     public next_state: any = 2;
@@ -107,6 +137,7 @@ export class TraveldocComponent implements OnInit {
         //5-hotel question, 6- hotel edition
         //7-advance question, 8- advance list, 9- advance edition
         //10-authorizacion request
+        this.doc_state_prev = this.doc_state;
         let temp_state = value;
         //console.log(this.current.traveldetail.length);
         if (temp_state == 1) {
@@ -161,6 +192,17 @@ export class TraveldocComponent implements OnInit {
             this.doc_state = temp_state;
             this.prev_state = 8;
             this.next_state = 0;
+        }
+    }
+
+    getState() {
+
+        if (this.doc_state > this.doc_state_prev) {
+            return 'right';
+        //} else if (this.doc_state > value) {
+        //    return 'right';
+        } else {
+            return 'left';
         }
 
     }
